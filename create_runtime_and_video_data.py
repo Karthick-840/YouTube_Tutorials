f7ae3a9ee701a60699fb3df8a3dd46b21c843aab
Karthick-840 <karthick.jayaraman@bayer.com>
2023-11-14 10:50:26 +0000

    Changed support_functions.py

--- create_runtime_and_video_data.py
+++ create_runtime_and_video_data.py
@@ -1,7 +1,39 @@
+from support_functions import convert_seconds_to_hms, extract_keywords
+
 from pytube import YouTube, Playlist
 import pandas as pd
 from datetime import timedelta
-from support_functions import convert_seconds_to_hms, extract_keywords
+
+
+
+def get_runtime_and_title(url):
+    if 'playlist' in url:
+        playlist = Playlist(url)
+        total_runtime = 0
+        video_count = 0
+        for video in playlist.videos:
+            total_runtime += video.length
+            video_count += 1
+            video_thumbnails.append(video.thumbnail_url)
+
+        average_playtime = total_runtime / video_count if video_count > 0 else 0
+
+        video_data = {
+            "Title": playlist.title,
+            "Average Playtime": convert_seconds_to_hms(average_playtime),
+            "Total Runtime": convert_seconds_to_hms(total_runtime),
+            "Video Count": video_count,
+            "Video Thumbnails": video_thumbnails,
+            "Video Link": url
+        }
+
+    else:
+        video = YouTube(url)
+        title = video.title
+        runtime = video.length
+        video_data = {"Title": title, "Run Time": convert_seconds_to_hms(runtime),"Video Count":1,"Video Link": url}
+   
+    return video_data
 
 
 def create_video_data(playlist_url):
